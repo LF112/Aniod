8f546de27acbe2fcb1cb08725cec1c7e5a4fc8da
“LF112” <lf@lf112.net>
2019-05-11 22:21:52 +0800

    Create aniod.js

---
+++ src/aniod.js
@@ -0,0 +1,30 @@
+/*
++----------------------------------------------------------
+|     _          _           _    _     
+|    / \   _ __ (_) ___   __| |  (_)___ 
+|   / _ \ | '_ \| |/ _ \ / _` |  | / __|
+|  / ___ \| | | | | (_) | (_| |_ | \__ \
+| /_/   \_\_| |_|_|\___/ \__,_(_)/ |___/
+|                              |__/     
+| Aniod - JavaScript-based elemental animation component.
+| 
+| # GitHub: https://github.com/LF112/Aniod
+| # Author: LF112「https://lf.tn」
+| # Version: V0.0.1
+| # Last Update: 2019.5.11
++----------------------------------------------------------
+*/
+
+class Aniod{
+
+    /**
+     * Aniod constructor function
+     *
+     * @param {Object} configuration
+     * @constructor
+     */
+    constructor (configuration) {
+        
+    }
+
+}
